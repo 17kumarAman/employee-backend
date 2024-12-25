552e538098de89b078c0fe744bb2bff779827606
17kumarAman <ak3722032@gmail.com>
2024-12-25 10:53:31 +0530

    s

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-const connectDB = require("./config/db");
-const jobRoutes = require('./routes/jobs');
-require("dotenv").config();
-
-const express = require("express");
-const cors = require("cors"); // Import CORS
+const express = require('express');
 const app = express();
+const PORT = 4000;
 
-// CORS configuration
-const corsOptions = {
-  origin: 'http://localhost:5173', // Allow requests from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type'], // Allowed headers
-};
+app.get('/', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+});
 
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
-// Enable CORS middleware
-app.use(cors(corsOptions));
-
-// Connect to MongoDB
-connectDB();
-
-app.use(express.json());
-
-// Your other routes and logic go here...
-app.use('/api', jobRoutes);
-app.get("/", (req, res) => res.send("Express on Vercel"));
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
